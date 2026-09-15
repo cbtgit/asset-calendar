4d1d2f6217a50306ef7046efacc9970b408752f9
Christian Thomsen <cbtgit@users.noreply.github.com>
2026-09-15 22:18:11 +0200

    Invite debug (#110)
    * Add debug for invite
    
    * Change order ofr reading smtp  config

--- pb_hooks/auth-config.pb.js
+++ pb_hooks/auth-config.pb.js
@@ -1,6 +1,6 @@
 onBootstrap((e) => {
   const authConfig = require(`${__hooks}/auth-config.cjs`);
   const configuration = authConfig.validateAuthConfig(authConfig.readPocketBaseEnvironment());
-  authConfig.configurePocketBaseMail($app, configuration);
   e.next();
+  authConfig.configurePocketBaseMail($app, configuration);
 });
