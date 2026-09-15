3b28996a63342c4c8c769fcec7deb3f68fde20dd
Christian Bloch Thomsen <cblochth@gmail.com>
2026-09-15 13:32:26 +0200

    Add Booking Types page layout and styles

--- src/routes/_authenticated/administration.booking-types.tsx
+++ src/routes/_authenticated/administration.booking-types.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/base/Button";
+import "./administration.booking-types.css";
 
 export const Route = createFileRoute("/_authenticated/administration/booking-types")({
   component: BookingTypesPage,
@@ -8,8 +10,13 @@ function BookingTypesPage() {
   return (
     <>
       <p className="eyebrow">Administration</p>
-      <h1>Booking Types</h1>
-      <p>Booking type administration will be available here.</p>
+      <div className="booking-types-header">
+        <div>
+          <h1>Booking Types</h1>
+          <p>Booking type administration will be available here.</p>
+        </div>
+        <Button variant="primary">New Booking Type</Button>
+      </div>
     </>
   );
 }
